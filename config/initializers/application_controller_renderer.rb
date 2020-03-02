6dd852dadf8e5b437b3603c0e1ef535b482a55fd
Shinkat0 <cntaro1234@gmail.com>
2020-03-02 07:16:42 +0000

    [finish] 3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7cefc6d4c0b3487aa79a5dfa0dcd97b1d50358af86c1ebda89326ad416f5a2dfa18a2002b02e08aa79bc62422ffca4ee817aec230d8f9aff4cf2b255059210b6'
